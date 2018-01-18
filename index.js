f65ace885df979821d42acd2d0bc4152116ed585
Yufang <yfeng93@terpmail.umd.edu>
2018-01-18 12:06:12 -0500

    testing conlog

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+console.log('fuck you fuck you')
