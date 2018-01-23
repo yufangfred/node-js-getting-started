6e0ce62dace9d07ba76a8fd9dece21ebddd0fde4
Yufang <yfeng93@terpmail.umd.edu>
2018-01-23 14:07:33 -0500

    local sign up from scotchio

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+// mongo ds111138.mlab.com:11138/heroku_q6h9ct0j -u YufangFeng -p 1oVeb\!ue
+module.exports = {
+
+    'url' : 'mongodb://YufangFeng:1oVeb!ue@ds111138.mlab.com:11138/heroku_q6h9ct0j' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
--- index.js
+++ index.js
@@ -19,32 +19,52 @@ var configDB = require('./config/database.js');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
+require('./config/passport')(passport); // pass passport for configuration
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+
+
+ app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index111'))
   
-  .get('/cool', (request, response) => response.send(cool())) 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//   .get('/cool', (request, response) => response.send(cool())) 
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
-app.get('/s',(req, res)=>{
+// app.get('/s',(req, res)=>{
 
-	res.render('pages/s');
-});
+// 	res.render('pages/s');
+// });
 
-app.get('/r', (req, res)=>{
-	var gosu = require('gosugamers-api');
-	var urls;
+// app.get('/r', (req, res)=>{
+// 	var gosu = require('gosugamers-api');
+// 	var urls;
 
-	gosu.fetchMatchUrls('lol', (err, urlsl) => {
+// 	gosu.fetchMatchUrls('lol', (err, urlsl) => {
 	
 		
-		urls = urlsl;
+// 		urls = urlsl;
 
-		res.render('pages/r.ejs' , {
-			urls: urls
-		});
-	});
-});
+// 		res.render('pages/r.ejs' , {
+// 			urls: urls
+// 		});
+// 	});
+// });
